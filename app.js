4b4214f3f3669c0dc123e0d9a0cae35b3214d673
Hassaan11 <saad117218@gmail.com>
2023-11-17 17:08:41 +0500

    Setup app

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express()
 
+const passportController = require("./controllers/passport.controller");
+const faceController = require("./controllers/face.controller");
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '15MB' }))
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+app.use("/api/passport", passportController);
+app.use("/api/face", faceController);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!')
 })
 
-
-
 app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`)
+  console.log(`Listening on port ${3000}`)
 })
